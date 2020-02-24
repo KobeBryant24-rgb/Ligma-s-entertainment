8d8f80aa29a411b0f5bc0ed5c42a487f3fca220f
KobeBryant24-rgb <60447822+KobeBryant24-rgb@users.noreply.github.com>
2020-02-24 12:46:22 -0800

    correccion

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Usuario
+{
+     class CorreoE{
+         private string correo;
+         public CorreoE(string c){
+             correo = c;
+         }
+         public string getCorreo(){
+             return correo;
+         }
+     }
+     class Usuario{
+         private string nombre;
+         private List <CorreoE> correos;
+         public Usuario(string nombre){
+             this.nombre = nombre;
+             correos = new List<CorreoE>();
+         }
+         public string getNombre(){
+             return nombre;
+         }
+         public void AgregaCorrero(CorreoE c){
+             correos.Add(c);
+         }
+         public void Print(){
+             Console.WriteLine(nombre);
+             foreach(CorreoE c in correos){
+                 Console.WriteLine(c.getCorreo());
+             }
+         }
+     }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario u1  = new Usuario("Juan");
+            Console.WriteLine(u1.getNombre());
+            u1.AgregaCorrero(new CorreoE("juan@.com"));
+            u1.AgregaCorrero(new CorreoE("juan@gmail.com"));
+            u1.Print();
+        }
+    }
+}
