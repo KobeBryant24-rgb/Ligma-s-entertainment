f1feadd581a64cd2a0002c60b164b03477bc2e67
KobeBryant24-rgb <60447822+KobeBryant24-rgb@users.noreply.github.com>
2020-03-02 12:51:46 -0800

    Title

---
+++ Examples/Actividad1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese dos numeros a evaluar");
+            Evaluar(Int32.Parse(Console.ReadLine()),Int32.Parse(Console.ReadLine()));
+           
+        }
+
+        ///Evaluar cual numero es mayor, menor o igual
+        public static int Evaluar(int numero1, int numero2){
+        if(numero1>numero2){
+            Console.WriteLine("Resultado");
+            Console.WriteLine(numero1 +  ">"  + numero2);
+        }
+        else
+        {
+            if(numero1<numero2){
+                Console.WriteLine("Resultado");
+                Console.WriteLine(numero1 +  "<"  + numero2);
+            }
+            else{
+                Console.WriteLine("Resultado");
+                Console.WriteLine(numero1 +  "="  + numero2);
+            }
+        }
+       
+
+           
+         return numero1;
+         
+
+         }
+         
+    }
+
+    
+}
